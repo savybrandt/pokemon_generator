c701430567a44b6c2aacb9abc93954c3204ebea4
Savy Brandt <savybrandt@Savys-Air.attlocal.net>
2021-09-08 18:23:47 -0400

    add: New Pokemon button

--- src/PokemonGenerator.js
+++ src/PokemonGenerator.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { usePreloadedQuery } from 'react-relay/hooks';
 import { PokemonQuery } from './api';
 
 const getRandomIndex = () => Math.floor(Math.random()*150)
 
 function PokemonGenerator ({ preloadedQuery }) {
+  const [pokemonNum, setPokemonNum] = useState(getRandomIndex())
   const data = usePreloadedQuery(PokemonQuery, preloadedQuery);
   const pokemons = data ? data.pokemons : null;
-  const pokemon = pokemons[getRandomIndex()];
+  const pokemon = pokemons[pokemonNum];
   
   if (pokemon) {
     return (
       <>
         <h1>{pokemon.name}</h1>
         <img alt={pokemon.name} src={pokemon.image}/>
+        <button onClick={() => setPokemonNum(getRandomIndex())}>New Pokemon!</button>
       </>
     )
   }
